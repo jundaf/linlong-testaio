05b5a8e1e96f3735d3ef3c467baf1f15b4815058
Junda Feng <jundaf@live.com>
2015-06-24 02:55:09 -0700

    initial version

---
+++ zmq-reqrep/hwclient.py
@@ -0,0 +1,28 @@
+#
+#   Hello World client in Python
+#   Connects REQ socket to tcp://localhost:5555
+#   Sends "Hello" to server, expects "World" back
+#
+
+import string
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server...")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+total_recieved = 0
+#  Do 26 requests, waiting each time for a response
+for request in string.ascii_lowercase * 1024:
+    #print("Sending request %s ..." % request)
+    socket.send_string(request)
+
+    #  Get the reply.
+    message = socket.recv()
+    total_recieved += len(message)
+    #print("Received reply %s [ %s ]" % (request, message))
+
+print("total recieved: {}".format(total_recieved))
---
+++ zmq-reqrep/mtserver.py
@@ -0,0 +1,83 @@
+"""
+
+   Multithreaded Hello World server
+
+   Author: Guillaume Aubert (gaubert) <guillaume(dot)aubert(at)gmail(dot)com>
+
+   Author: Junda Feng
+
+"""
+import time
+import threading
+import zmq
+
+WORKER_NUM = 5
+DEFAULT_SIZE = 1024
+
+BUFFERS = {}
+
+
+def setup_buffers():
+    import sys, string
+    buffer_size = DEFAULT_SIZE
+    if len(sys.argv) >= 2:
+        buffer_size = int(sys.argv[1])
+
+    print("set buffer size to {}".format(buffer_size))
+
+    for letter in string.ascii_lowercase:
+        BUFFERS[letter] = letter * buffer_size
+
+
+def worker_routine(worker_url, context=None):
+    """Worker routine"""
+    context = context or zmq.Context.instance()
+    # Socket to talk to dispatcher
+    socket = context.socket(zmq.REP)
+    socket.connect(worker_url)
+
+    while True:
+        try:
+            string_ = socket.recv_string()
+            #send reply back to client
+            socket.send_string(BUFFERS[string_])
+        except zmq.ContextTerminated:
+            break
+
+def main():
+    """Server routine"""
+
+    url_worker = "inproc://workers"
+    url_client = "tcp://*:5555"
+
+    setup_buffers()
+
+    # Prepare our context and sockets
+    context = zmq.Context.instance()
+
+    # Socket to talk to clients
+    clients = context.socket(zmq.ROUTER)
+    clients.bind(url_client)
+
+    # Socket to talk to workers
+    workers = context.socket(zmq.DEALER)
+    workers.bind(url_worker)
+
+    # Launch pool of worker threads
+    for i in range(WORKER_NUM):
+        thread = threading.Thread(target=worker_routine, args=(url_worker,))
+        thread.start()
+
+    try:
+        zmq.device(zmq.QUEUE, clients, workers)
+    except KeyboardInterrupt:
+        pass
+
+    # We never get here but clean up anyhow
+    clients.close()
+    workers.close()
+    context.term()
+
+
+if __name__ == "__main__":
+    main()
